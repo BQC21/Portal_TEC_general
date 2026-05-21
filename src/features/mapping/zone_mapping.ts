19e4ef51d280edd7420ba7ab75df91ef52b806e0
Braulio <braulio.quine@utec.edu.pe>
2026-05-21 15:42:45 -0500

    add link to connect zone atributes with zone Form and send Form info to update Supabase table

--- src/features/mapping/zone_mapping.ts
+++ src/features/mapping/zone_mapping.ts
@@ -1,5 +1,19 @@
-import {Zone, SupabaseZoneRow} from "@/lib/types/zone-types"
+import {Zone, ZoneFormState, ZoneFormData, SupabaseZoneRow} from "@/lib/types/zone-types"
 import { parseNullableDate } from "@/lib/utils/helpers"
+
+// enlace con los atributos de Supabase
+export function createProjectFormStateFromProject(zone: Zone): ZoneFormState {
+    return {
+        zona: zone.zona,
+        // cálculos de radiación
+        latitude: zone.latitude,
+        longitude: zone.longitude,
+        ghi_respaldo: zone.ghi_respaldo,
+        // fechas
+        created_at: zone.created_at,
+        updated_at: zone.updated_at,
+    }
+}
 
 /*
     Lectura de la base de datos de Supabase
@@ -19,4 +33,22 @@ export function mapSupabaseRowToZone(
         created_at: parseNullableDate(row.created_at) ?? new Date(),
         updated_at: parseNullableDate(row.updated_at) ?? new Date(),
 	}
+}
+
+/*
+    Envío de datos a la base de datos de Supabase
+*/
+export function mapProjectToSupabaseRow(
+    zone: ZoneFormData
+): SupabaseZoneRow {
+    return {
+        zona: zone.zona,
+        // cálculos de radiación
+        latitude: zone.latitude as unknown as number | undefined,
+        longitude: zone.longitude as unknown as number | undefined,
+        ghi_respaldo: zone.ghi_respaldo as unknown as number | undefined,
+        // fechas
+        created_at: zone.created_at,
+        updated_at: zone.updated_at,
+    }
 }
