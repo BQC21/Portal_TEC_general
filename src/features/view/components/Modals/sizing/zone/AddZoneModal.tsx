cf31edf43ef4278a44854ff7794126b911ccc2d5
Braulio <braulio.quine@utec.edu.pe>
2026-06-04 10:07:51 -0500

    add new fields to Add zone & edit zone modals

--- src/features/view/components/Modals/sizing/zone/AddZoneModal.tsx
+++ src/features/view/components/Modals/sizing/zone/AddZoneModal.tsx
@@ -14,8 +14,6 @@ import { AddProductTextField } from "@/features/view/components//Form_fields/Add
 import { INITIAL_ZONE_FORM } from "@/lib/utils/initialValues";
 import { TABLE_HEADERS_ZONE } from "@/lib/utils/headers";
 
-import { useZone } from "@/features/view/hooks/services/useRealtimeZonas";
-
 // --- Tipo de variables ---
 type AddZoneModalProps = {
     onAddZone: (zone: ZoneFormData) => void;
@@ -23,8 +21,6 @@ type AddZoneModalProps = {
 };
 
 export default function AddZoneModal({ onAddZone, onClose }: AddZoneModalProps) {
-    const { zones } = useZone(); // obtener la lista de zonas
-
     const [form_zone, setForm_zone] = useState<ZoneFormState>(INITIAL_ZONE_FORM);
 
     function updateField<K extends keyof ZoneFormState>(field: K, value: ZoneFormState[K]) {
@@ -60,7 +56,6 @@ export default function AddZoneModal({ onAddZone, onClose }: AddZoneModalProps) 
                 <form onSubmit={handleSubmit} className="max-h-[calc(95vh-88px)] overflow-y-auto px-6 py-6">
                     <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-3 py-5 sm:px-6 lg:px-8">
                         <section className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
-                            {/* Nombre de la zona */}
                             <AddProductTextField
                                 label={TABLE_HEADERS_ZONE[0]}
                                 required
@@ -82,12 +77,35 @@ export default function AddZoneModal({ onAddZone, onClose }: AddZoneModalProps) 
                                 value={form_zone.longitude}
                                 onChange={(value) => updateField("longitude", value)}
                             />                            
+                        </section>
+                        <section className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
                             <AddProductTextField
                                 label={TABLE_HEADERS_ZONE[3]}
                                 required
                                 placeholder=" "
                                 value={form_zone.ghi_respaldo}
                                 onChange={(value) => updateField("ghi_respaldo", value)}
+                            />
+                                <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[4]}
+                                required
+                                placeholder=" "
+                                value={form_zone.ghi_respaldo_diario}
+                                onChange={(value) => updateField("ghi_respaldo_diario", value)}
+                            />
+                            <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[5]}
+                                required
+                                placeholder=" "
+                                value={form_zone.gti_respaldo}
+                                onChange={(value) => updateField("gti_respaldo", value)}
+                            />
+                            <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[6]}
+                                required
+                                placeholder=" "
+                                value={form_zone.gti_respaldo_diario}
+                                onChange={(value) => updateField("gti_respaldo_diario", value)}
                             />
                         </section>
                     </div>
--- src/features/view/components/Modals/sizing/zone/EditZoneModal.tsx
+++ src/features/view/components/Modals/sizing/zone/EditZoneModal.tsx
@@ -14,8 +14,6 @@ import { AddProductTextField } from "@/features/view/components/Form_fields/AddT
 
 import { TABLE_HEADERS_ZONE } from "@/lib/utils/headers";
 
-import { useZone } from "@/features/view/hooks/services/useRealtimeZonas";
-
 import { createZoneFormStateFromZone } from "@/lib/mapping/zone_mapping"; 
 
 type EditZoneModalProps = {
@@ -25,8 +23,6 @@ type EditZoneModalProps = {
 };
 
 export default function EditZoneModal({ existingZone, onUpdateZone, onClose }: EditZoneModalProps) {
-    const { zones } = useZone();
-
     const [form_zone, setForm_zone] = useState<ZoneFormState>(() => createZoneFormStateFromZone(existingZone));;
     
     function updateField<K extends keyof ZoneFormState>(field: K, value: ZoneFormState[K]) {
@@ -56,7 +52,6 @@ export default function EditZoneModal({ existingZone, onUpdateZone, onClose }: E
                 <form onSubmit={handleSubmit} className="max-h-[calc(95vh-88px)] overflow-y-auto px-6 py-6">
                     <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-3 py-5 sm:px-6 lg:px-8">
                         <section className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
-                            {/* Nombre de la zona */}
                             <AddProductTextField
                                 label={TABLE_HEADERS_ZONE[0]}
                                 required
@@ -78,12 +73,35 @@ export default function EditZoneModal({ existingZone, onUpdateZone, onClose }: E
                                 value={form_zone.longitude}
                                 onChange={(value) => updateField("longitude", value)}
                             />                            
+                        </section>
+                        <section className="flex flex-col gap-4 lg:flex-row lg:items-start lg:justify-between">
                             <AddProductTextField
                                 label={TABLE_HEADERS_ZONE[3]}
                                 required
                                 placeholder=" "
                                 value={form_zone.ghi_respaldo}
                                 onChange={(value) => updateField("ghi_respaldo", value)}
+                            />
+                                <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[4]}
+                                required
+                                placeholder=" "
+                                value={form_zone.ghi_respaldo_diario}
+                                onChange={(value) => updateField("ghi_respaldo_diario", value)}
+                            />
+                            <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[5]}
+                                required
+                                placeholder=" "
+                                value={form_zone.gti_respaldo}
+                                onChange={(value) => updateField("gti_respaldo", value)}
+                            />
+                            <AddProductTextField
+                                label={TABLE_HEADERS_ZONE[6]}
+                                required
+                                placeholder=" "
+                                value={form_zone.gti_respaldo_diario}
+                                onChange={(value) => updateField("gti_respaldo_diario", value)}
                             />
                         </section>
                     </div>
