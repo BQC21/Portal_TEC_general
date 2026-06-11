49b1cffa0ebb01643dbc079849d176cf3baa28d6
Braulio <braulio.quine@utec.edu.pe>
2026-06-11 13:48:15 -0500

    add computing energy requirements

--- src/lib/utils/helpers/computes/energy_requirements.ts
+++ src/lib/utils/helpers/computes/energy_requirements.ts
@@ -10,6 +10,26 @@ export function compute_DC_Power(energia: number, ghi: number, eficiencia: numbe
 	return energia / (ghi * eficiencia/100);
 }
 
-export function compute_AC_Power(potenciaDC: number, DC_AC: number) {
-	return potenciaDC / (DC_AC/100);
+export function compute_AC_Power(potenciaDC: number) {
+	return potenciaDC / 1.2;
+}
+
+export function min_strings(potenciaDC: number, potenciaMOD: number){
+	return potenciaDC/potenciaMOD;
+}
+
+export function max_strings(potenciaDC_INV: number, potenciaMOD: number){
+	return potenciaDC_INV/potenciaMOD;
+}
+
+export function ITM_DC_MIN(isc_mod: number){
+	return Math.floor(isc_mod*1.25)
+}
+
+export function ITM_AC_MIN(iout_inv: number){
+	return Math.ceil(iout_inv*1.25)
+}
+
+export function SPD_MIN(string_number: number, voc_mod: number, mppt_number: number){
+	return Math.ceil(string_number * voc_mod/mppt_number)
 }
