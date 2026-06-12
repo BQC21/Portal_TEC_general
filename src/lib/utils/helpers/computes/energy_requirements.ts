20747d84f40c26c6dba2ff361b6cda257a23bcc1
Braulio <braulio.quine@utec.edu.pe>
2026-06-12 10:07:33 -0500

    add functions to compute system Ah & number of necessary batteries

--- src/lib/utils/helpers/computes/energy_requirements.ts
+++ src/lib/utils/helpers/computes/energy_requirements.ts
@@ -1,5 +1,5 @@
 // -------------------------
-// Funciones para el cálculo de requerimientos energéticos
+// Requerimientos energéticos
 // -------------------------
 
 export function computeEnergy(demanda: number, cobertura: number) {
@@ -14,6 +14,10 @@ export function compute_AC_Power(potenciaDC: number) {
 	return potenciaDC / 1.2;
 }
 
+// -------------------------
+// Campo fotovoltaico
+// -------------------------
+
 export function min_strings(potenciaDC: number, potenciaMOD: number){
 	return potenciaDC/potenciaMOD;
 }
@@ -21,6 +25,10 @@ export function min_strings(potenciaDC: number, potenciaMOD: number){
 export function max_strings(potenciaDC_INV: number, potenciaMOD: number){
 	return potenciaDC_INV/potenciaMOD;
 }
+
+// -------------------------
+// Protecciones
+// -------------------------
 
 export function ITM_DC_MIN(isc_mod: number){
 	return Math.floor(isc_mod*1.25)
@@ -30,6 +38,20 @@ export function ITM_AC_MIN(iout_inv: number){
 	return Math.ceil(iout_inv*1.25)
 }
 
-export function SPD_MIN(string_number: number, voc_mod: number, mppt_number: number){
+export function SPD_MIN(string_number: number, voc_mod: number, 
+	mppt_number: number){
 	return Math.ceil(string_number * voc_mod/mppt_number)
+}
+
+// -------------------------
+// Almacenamiento energético
+// -------------------------
+
+export function AH_sistema(demanda: number, autonomía: number, 
+	dod: number, voltaje_bateria: number){
+	return (demanda*autonomía)/(dod*voltaje_bateria)
+}
+
+export function N_baterias(ah_sistema: number, ah_bateria: number){
+	return ah_sistema/ah_bateria
 }
