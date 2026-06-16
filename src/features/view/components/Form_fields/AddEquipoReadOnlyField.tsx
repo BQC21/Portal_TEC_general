79ca8914374c2d686804d3eb6118ab3dfe546dd7
Braulio <braulio.quine@utec.edu.pe>
2026-06-16 15:41:16 -0500

    add colorClass as prop on ReadOnlyField component

--- src/features/view/components/Form_fields/AddEquipoReadOnlyField.tsx
+++ src/features/view/components/Form_fields/AddEquipoReadOnlyField.tsx
@@ -3,13 +3,15 @@ import { AddProductFieldLabel } from "./AddFieldLabel";
 type AddEquipoReadonlyFieldProps = {
     label: string;
     value: string;
+    colorClass?: string; // Optional color class for conditional styling
 };
 
-export function AddEquipoReadonlyField({ label, value }: AddEquipoReadonlyFieldProps) {
+export function AddEquipoReadonlyField({ label, value, colorClass = "text-slate-700" }: AddEquipoReadonlyFieldProps) {
     return (
         <div>
         <AddProductFieldLabel label={label} />
-        <div className="rounded-xl border border-slate-200 bg-slate-300 px-4 py-3 text-lg text-slate-700">{value}</div>
+        <div className={`
+            rounded-xl border border-slate-200 ${colorClass} px-4 py-3 text-lg`}>{value}</div>
         </div>
     );
 }
--- src/features/view/components/Form_fields/AddReadonlyField.tsx
+++ src/features/view/components/Form_fields/AddReadonlyField.tsx
@@ -3,13 +3,14 @@ import { AddProductFieldLabel } from "./AddFieldLabel";
 type AddProductReadonlyFieldProps = {
     label: string;
     value: string;
+    colorClass?: string; // Optional color class for conditional styling
 };
 
-export function AddProductReadonlyField({ label, value }: AddProductReadonlyFieldProps) {
+export function AddProductReadonlyField({ label, value, colorClass = "text-slate-700" }: AddProductReadonlyFieldProps) {
     return (
         <div>
         <AddProductFieldLabel label={label} />
-        <div className="rounded-xl border border-slate-200 bg-slate-100 px-4 py-3 text-lg text-slate-700">{value}</div>
+        <div className={`rounded-xl border border-slate-200 ${colorClass} px-4 py-3 text-lg`}>{value}</div>
         </div>
     );
 }
