32303cd0a7e61d12c98070df6aab6ec058ea6230
Braulio <braulio.quine@utec.edu.pe>
2026-06-25 11:48:12 -0500

    refactor selector names used on project modals

---
+++ src/lib/utils/helpers/project_modals/rows.ts
@@ -0,0 +1,15 @@
+export const equipmentRows = [
+    "ACCESORIO",
+    "BATERÍA",
+    "ESTRUCTURA",
+    "INVERSOR",
+    "MÓDULO FV",
+];
+
+export const materialRows = [
+    "CABLE",
+    "PROTECCIÓN",
+    "MC4",
+    "CANALIZACIÓN",
+    "CONSUMIBLE",
+];
