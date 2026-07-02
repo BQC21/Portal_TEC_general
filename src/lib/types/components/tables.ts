26cd58670a6e2013995b87a6d301895309a10684
Braulio <braulio.quine@utec.edu.pe>
2026-07-02 17:15:37 -0500

    add typing to new table components

--- src/lib/types/components/tables.ts
+++ src/lib/types/components/tables.ts
@@ -1,9 +1,12 @@
+import { Brand } from "../supabase/brand.types";
 import { Equipos } from "../supabase/equipos-types";
 import { Materiales } from "../supabase/materiales-types";
 import { Product, SelectedEquipmentItem, SelectedMaterialItem } from "../supabase/product-types";
 import { Project } from "../supabase/project-types";
 import { Project_Equipos } from "../supabase/project_equipos_join";
 import { Project_Materiales } from "../supabase/project_materiales_join";
+import { Supplier } from "../supabase/supplier-types";
+import { Type } from "../supabase/type-types";
 import { Zone } from "../supabase/zone-types";
 
 // zonas
@@ -53,4 +56,28 @@ export type EquiposTableProps = {
     totalEquipos: number;
     onUpdateEquipos: (equipo: Equipos) => void;
     onDeleteEquipos: (equipoId: string) => void;
+};
+
+// proveedores
+export type ProveedoresTableProps = {
+    supplier: Supplier[];
+    totalEquipos: number;
+    onUpdateSupplier: (supplier: Supplier) => void;
+    onDeleteSupplier: (supplierId: string) => void;
+};
+
+// marcas
+export type MarcasTableProps = {
+    brand: Brand[];
+    totalEquipos: number;
+    onUpdateBrand: (brand: Equipos) => void;
+    onDeleteBrand: (brandId: string) => void;
+};
+
+// tipos
+export type TiposTableProps = {
+    type: Type[];
+    totalType: number;
+    onUpdateType: (type: Type) => void;
+    onDeleteType: (typeId: string) => void;
 };
