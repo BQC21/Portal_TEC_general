a34f7497a87fee736d796ed1a231be3edc544739
Braulio <braulio.quine@utec.edu.pe>
2026-07-14 17:09:44 -0500

    add new typing for summary tables

--- src/lib/types/components/Quotes/finantial_computes.ts
+++ src/lib/types/components/Quotes/finantial_computes.ts
@@ -26,8 +26,36 @@ export type ViaticosCostsInput = {
     courierTotalIgv: number;
 };
 
+export type RecursosSubtotalInput = {
+    subtotal_recursos: number;
+    margenRiesgo_recursos: number;
+    subtotalConMargenRiesgo_recursos: number;
+    markUp_recursos: number;
+    ventaSoles_recursos: number;
+    ventaSolesIgv_recursos: number;
+    ventaDolares_recursos: number;
+    ventaDolaresIgv_recursos: number;
+};
+
+export type ViaticosSubtotalInput = {
+    subtotal_viaticos: number;
+    margenRiesgo_viaticos: number;
+    subtotalConMargenRiesgo_viaticos: number;
+    markUp_viaticos: number;
+    ventaSoles_viaticos: number;
+    ventaSolesIgv_viaticos: number;
+    ventaDolares_viaticos: number;
+    ventaDolaresIgv_viaticos: number;   
+};
+
 export type SummaryCostTable1_props = RecursosCostsInput & {
     gm_general: number;
     markup: number;
     tasa_cambio: number;
-}
+} & RecursosSubtotalInput;
+
+export type SummaryCostTable2_props = ViaticosCostsInput & {
+    gm_general: number;
+    markup: number;
+    tasa_cambio: number;
+} & ViaticosSubtotalInput;
