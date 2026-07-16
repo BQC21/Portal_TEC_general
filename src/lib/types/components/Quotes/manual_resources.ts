dd279d94ff1bf5d63399bc05eef775ccd7e5251f
Braulio <braulio.quine@utec.edu.pe>
2026-07-16 10:43:11 -0500

    update typing data to save manual costs

--- src/lib/types/components/Quotes/manual_resources.ts
+++ src/lib/types/components/Quotes/manual_resources.ts
@@ -1,12 +1,51 @@
-export type ManualResourceCosts = {
-    epp: { descripcion: string; cantidad: number; precio_unitario: number };
-    tooling: { descripcion: string; cantidad: number; precio_unitario: number };
-    hotel: { monto: number; personas: number; dias: number };
-    personal: { nombre: string; puesto: string; dias: number; precio_dia: number };
-    sctr: { descripcion: string; cantidad: number; precio_unitario: number };
-    eating: { monto: number; personas: number; dias: number };
-    traveling: { monto: number; personas: number; dias: number };
-    courier: { descripcion: string; cantidad: number; precio_unitario: number };
+
+// Añadir patrón de cantidad
+export type QuantityPriceItem = {
+    id: string;
+    descripcion: string;
+    cantidad: number;
+    precio_unitario: number;
 };
+export const EMPTY_QUANTITY_PRICE_ITEM: Omit<QuantityPriceItem, "id"> = { 
+    descripcion: "", cantidad: 0, precio_unitario: 0 };
 
 
+// Añadir patrón de personal
+export type PersonalItem = {
+    id: string;
+    nombre: string;
+    puesto: string;
+    dias: number;
+    precio_dia: number;
+};
+export const EMPTY_PERSONAL_ITEM: Omit<PersonalItem, "id"> = { 
+    nombre: "", puesto: "", dias: 0, precio_dia: 0 };
+
+
+
+// Añadir patrón de montos
+export type MontoItem = {
+    monto: number;
+    personas: number;
+    dias: number;
+}
+
+
+
+// ---------------------------
+// Asignación manual de costos
+// ---------------------------
+export type ManualCosts = {
+    Recursos: {
+        epp: QuantityPriceItem[];
+        tooling: QuantityPriceItem[];
+        personal: PersonalItem[];
+        sctr: QuantityPriceItem[];
+        hotel: MontoItem;
+    };
+    Viaticos: {
+        eating: MontoItem;
+        traveling: MontoItem;
+        courier: QuantityPriceItem[];
+    }
+};
