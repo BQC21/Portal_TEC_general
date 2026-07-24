c55fc3e70b1bd182e04a6f172749aad231b48c1b
Braulio <braulio.quine@utec.edu.pe>
2026-07-24 10:22:47 -0500

    add eye icons as Form fields components

---
+++ src/features/view/components/Form_fields/EyeSlashIcon.tsx
@@ -0,0 +1,8 @@
+export const EyeSlashIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18Zm10.94 13.47a.75.75 0 0 0 1.06-.01l5.97-5.97-1.06-1.06-5.97 5.97a.75.75 0 0 0 .01 1.06Z" clipRule="evenodd" />
+            <path d="M11.25 11.25a.75.75 0 0 0-1.5 0v2.25H7.5a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25h2.25a.75.75 0 0 0 0-1.5h-2.25V7.5a.75.75 0 0 0-1.5 0v2.25H7.5a.75.75 0 0 0 0 1.5h2.25v2.25Z" />
+        </svg>
+    )
+}
