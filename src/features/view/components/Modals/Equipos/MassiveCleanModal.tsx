392922eef2d8db78ed99f6e47698e56be5ed1f35
Braulio <braulio.quine@utec.edu.pe>
2026-07-31 17:21:39 -0500

    hide massive clean password

--- src/features/view/components/Modals/Equipos/MassiveCleanModal.tsx
+++ src/features/view/components/Modals/Equipos/MassiveCleanModal.tsx
@@ -7,8 +7,7 @@ import { AddProductTextField } from "@/features/view/components/Form_fields/AddT
 import { EQUIPOS_TABLE } from "@/lib/utils/namingTolerance";
 import { bulkDeleteAllRows } from "@/lib/utils/helpers/massive/massiveClean";
 import { MassiveCleanModalProps } from "@/lib/types/components/modals";
-
-const CONFIRM_PASSWORD = "LIMPIEZA";
+import { CONFIRM_PASSWORD } from "@/passwords/keys";
 
 export function MassiveCleanModal({ currentCount, onClose, onSuccess }: MassiveCleanModalProps) {
 
