bc90d3fb711a67bf94a65d216296d85f3bfcdeb1
Braulio <braulio.quine@utec.edu.pe>
2026-08-10 10:32:02 -0500

    Prepare fixed templates to agilize quote process

---
+++ src/features/view/sub_components/M3/Tables/quotes/templates/Prices.ts
@@ -0,0 +1,69 @@
+// --- EPP
+
+export const EPP_template = [
+    {id: 1, descripcion: "Cascos", cantidad: 4, precio_unitario: 35.00},
+    {id: 2, descripcion: "Mentonera", cantidad: 4, precio_unitario: 5.00},
+    {id: 3, descripcion: "Guantes Anticorte", cantidad: 8, precio_unitario: 12.00},
+    {id: 4, descripcion: "Guantes Badana", cantidad: 4, precio_unitario: 18.00},
+    {id: 5, descripcion: "Guantes Dieléctricos", cantidad: 1, precio_unitario: 250.00},
+    {id: 6, descripcion: "Lentes Oscuros", cantidad: 8, precio_unitario: 15.00},
+    {id: 7, descripcion: "Lentes Claros", cantidad: 0, precio_unitario: 10.00},
+    {id: 8, descripcion: "Chalecos", cantidad: 4, precio_unitario: 50.00},
+    {id: 9, descripcion: "Tapones Auditivos", cantidad: 8, precio_unitario: 3.00},
+    {id: 10, descripcion: "Tapasol", cantidad: 8, precio_unitario: 12.00},
+    {id: 11, descripcion: "Protector Solar", cantidad: 1, precio_unitario: 160.00},
+    {id: 12, descripcion: "Arnés con línea de vida", cantidad: 1, precio_unitario: 250.00},
+    {id: 13, descripcion: "Línea de vida anclaje", cantidad: 1, precio_unitario: 80.00},
+    {id: 14, descripcion: "Puntos de anclaje fijos", cantidad: 1, precio_unitario: 30.00},
+    {id: 15, descripcion: "Careta Antiarco", cantidad: 0.5, precio_unitario: 600.00},
+    {id: 16, descripcion: "Botas Dieléctricas", cantidad: 1, precio_unitario: 200.00},
+    {id: 17, descripcion: "Botas Antiarco", cantidad: 0.2, precio_unitario: 1000.00},
+];
+
+// --- Tooling
+export const Tooling_template = [
+    {id: 1, descripcion: "Máquina de soldar", cantidad: 1, precio_unitario: 1500.00},
+    {id: 2, descripcion: "JGO de alicates aislados 1000 V", cantidad: 2, precio_unitario: 200.00},
+    {id: 3, descripcion: "JGO de destornilladores aislados 1000 V", cantidad: 2, precio_unitario: 200.00},
+    {id: 4, descripcion: "Prensador de terminales hidraúlicos", cantidad: 2, precio_unitario: 300.00},
+    {id: 5, descripcion: "Taladro percutor inalámbrico", cantidad: 1, precio_unitario: 2500.00},
+    {id: 6, descripcion: "Taladro inalámbrico", cantidad: 1, precio_unitario: 1200.00},
+    {id: 7, descripcion: "Amoladora inalámbrica", cantidad: 1, precio_unitario: 900.00},
+    {id: 8, descripcion: "JGO de llaves mixtas", cantidad: 1, precio_unitario: 120.00},
+    {id: 9, descripcion: "JGO de dados", cantidad: 2, precio_unitario: 200.00},
+    {id: 10, descripcion: "JGO de llaves Allen", cantidad: 2, precio_unitario: 60.00},
+    {id: 11, descripcion: "Soga x metro", cantidad: 2, precio_unitario: 7.50},
+    {id: 12, descripcion: "Polea", cantidad: 1, precio_unitario: 500.00},
+    {id: 13, descripcion: "Carro para transportar paneles", cantidad: 1, precio_unitario: 350.00},
+    {id: 14, descripcion: "Prensador de MC4", cantidad: 1, precio_unitario: 120.00},
+    {id: 15, descripcion: "Prensador de terminales mecánico", cantidad: 1, precio_unitario: 150.00},
+    {id: 16, descripcion: "Soga 5/8 50 MTS", cantidad: 1, precio_unitario: 120.00},
+    {id: 17, descripcion: "Laser verde", cantidad: 1, precio_unitario: 600.00},
+    {id: 18, descripcion: "Telurómetro", cantidad: 1, precio_unitario: 700.00},
+    {id: 19, descripcion: "Pinza amperimética", cantidad: 1, precio_unitario: 200.00},
+    {id: 20, descripcion: "Pistola aplicadora de SICA", cantidad: 1, precio_unitario: 201.00},
+    {id: 21, descripcion: "Entornillador Dewalt", cantidad: 1, precio_unitario: 990.00},
+    {id: 22, descripcion: "Maleta de herramientas", cantidad: 1, precio_unitario: 1000.00},
+];
+
+// --- SCTR
+export const SCTR_template = [
+    {id: 1, descripcion: "SCTR SALUD Y PENSIÓN", cantidad: 1, precio_unitario: 500.00},
+    {id: 2, descripcion: "SCTR VIDA LEY", cantidad: 1, precio_unitario: 145.83},
+];
+
+// --- Courier
+export const Courier_template = [
+    {id: 1, descripcion: "Envío de equipos principales", cantidad: 1, precio_unitario: 1200.00},
+    {id: 2, descripcion: "Envío de herramientas y consumibles", cantidad: 1, precio_unitario: 400.00},
+];
+
+
+// --- Personal
+export const Personal_template = [
+    {id: 1, nombre: "Jesús Arteaga", puesto: "Gerente de Operaciones", dias: 10, precio_dia: 300.00},
+    {id: 2, nombre: "Lorena Calderón", puesto: "Analista de Ingeniería", dias: 10, precio_dia: 150.00},
+    {id: 3, nombre: "Roberto Dioses", puesto: "Coordinador de Proyectos", dias: 10, precio_dia: 150.00},
+    {id: 4, nombre: "Pedro Castillo", puesto: "Técnico", dias: 10, precio_dia: 150.00},
+    {id: 5, nombre: "Rolando", puesto: "Técnico", dias: 10, precio_dia: 100.00},
+];
