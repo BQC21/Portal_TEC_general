b8eea1e488cd114edb6c9c7fa25126a1d62427a3
Braulio <braulio.quine@utec.edu.pe>
2026-08-14 11:21:16 -0500

    add consumeItem Pattern and it empty values

--- src/lib/types/components/Quotes/manual_resources.ts
+++ src/lib/types/components/Quotes/manual_resources.ts
@@ -36,12 +36,27 @@ export const EMPTY_MONTO_ITEM: MontoItem = {
 };
 
 
+// Añadir patrón de consumibles
+export type ConsumeItem = {
+    id: string;
+    cod_producto: string;
+    descripcion: string;
+    tipo_de_producto: string;
+    cantidad: number;
+};
+export const EMPTY_CONSUME_ITEM: Omit<ConsumeItem, "id"> = {
+    cod_producto: "",
+    descripcion: "",
+    tipo_de_producto: "CONSUMIBLE",
+    cantidad: 0,
+};
 
 // ---------------------------
 // Asignación manual de costos
 // ---------------------------
 export type ManualCosts = {
     Recursos: {
+        consumible: ConsumeItem[];
         epp: QuantityPriceItem[];
         tooling: QuantityPriceItem[];
         personal: PersonalItem[];
