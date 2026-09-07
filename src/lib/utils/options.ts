be9663d894604fcbefdd39eb9f10cea4f92bdf42
Braulio <braulio.quine@utec.edu.pe>
2026-09-07 14:43:03 -0500

    update template for default pay content to display on quote report

--- src/lib/utils/options.ts
+++ src/lib/utils/options.ts
@@ -165,6 +165,6 @@ export const EPP_REUSABLE_OPTIONS = [
 export const DSCTO_type_value = ["Porcentaje", "USD"]
 
 export const DEFAULT_PAY_FORMAT =
-    "50% Con la orden de servicio\n50% Al término de instalación";
+    "60% Con la Orden de Servicio\n30% Antes del envío de los equipos\n20% Al término de la instalación";
 
 export const FIRM_OPTION: FIRMOptions[] = ["SIN FIRMA", "CON FIRMA"];
